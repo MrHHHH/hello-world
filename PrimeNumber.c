0a9c64e0c4d2444fa8453a4239c0f3a4b17e8f41
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-13 23:50:39 +0800

    Prime Number
    Find the prime number from 100 to 200.

---
+++ PrimeNumber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int count = 0;
+	int i = 0, j = 0;
+	for (i = 101; i < 200; i += 2) {
+		int flag = 0;
+		for (j = 2; j < sqrt(i); j++) {
+			if (i%j == 0) {
+				flag = 1;
+			}
+			if (flag == 1) {
+				break;
+			}
+		}
+		if (flag == 0) {
+			printf("%d\t", i);
+			count++;
+		}
+	}
+	printf("\ncount:%d\n", count);
+	return 0;
+}
