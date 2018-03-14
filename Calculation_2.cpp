f9b099893f10ad08b181685b618d50bef30a9f42
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 00:40:49 +0800

    Calculation -2
    How many times the number 9 appears in all integers form 1 to 100.

---
+++ Calculation_2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int count = 0;
+	int i = 0;
+	for (i = 9; i < 100; i++) {
+		int flag = 0;
+		if (i % 10 == 9) {
+			count++;
+			flag++;
+		}
+		if (i / 10 == 9) {
+			count++;
+			flag++;
+		}
+		if (flag) {
+			printf("%d\t", i);
+		}
+	}
+	printf("\ncount: %d\n", count);
+	return 0;
+}
