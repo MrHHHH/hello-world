ce7b5d5f7ac3b0b086984d82e619c20295ac7b2c
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-14 23:08:45 +0800

    Swap - 2
    Swapping two numbers with bit operations.

---
+++ Swap_2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define SWAP(a,b) { a = a ^ b; b = a ^ b; a = a ^ b; }
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d%d", &a, &b);
+	printf("before: a = %d  b = %d\n", a, b);
+	SWAP(a, b);
+	printf("after: a = %d  b = %d\n", a, b);
+	return 0;
+}
