2b767d6f4d8319a0433ddf532833860ada859cb4
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 00:22:17 +0800

    Calculation - 1
    Calculation 1-1/2+1/3-1/4······+1/99-1/100.

---
+++ Calculation_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	double sum = 1;
+	for (i = 2; i <= 100; i++) {
+		if (i % 2 != 0) {
+			sum += (1.0 / i);
+		}
+		else {
+			sum -= (1.0 / i);
+		}
+	}
+	printf("sum = %f\n", sum);
+	return 0;
+}
