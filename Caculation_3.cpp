c7c06fa5a540b6a8664d1da8b6ee300207c2bef6
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 01:13:05 +0800

    Caculation - 3
    Sn = a + aa + aaa + aaaa + aaaaa

---
+++ Caculation_3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int sn = 0;
+	int i = 0, t = 0;
+	scanf("%d", &a);
+	for (i = 0; i < 5; i++) {
+		t = t * 10 + a;
+		sn += t;
+	}
+	printf("Sn = %d\n", sn);
+	return 0;
+}
