a7be8c6731cd3b10881495b55d3c70736639850b
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 22:46:55 +0800

    Compare Bits
    How many bits are different in the binary representation of two int (32-bit) integers m and n

---
+++ Compare_Bits.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int count_one_bits(int n)
+{
+	int count = 0;
+	while (n) {
+		n = n&(n - 1);
+		count++;
+	}
+	return count;
+}
+int main()
+{
+	int a = 0, b = 0;
+	scanf("%d%d", &a, &b);
+	int n = a ^ b;
+	int count = count_one_bits(n);
+	printf("%d\n", count);
+	return 0;
+}
