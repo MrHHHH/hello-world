357217d169f7a2f8da7e751c2f8a230d908e3e0b
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 23:02:39 +0800

    Multiplication

---
+++ Multiplication_2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void print(int num)
+{
+	int i = 0, j = 0;
+	for (i = 1; i <= num; i++) {
+		for (j = 1; j <= i; j++) {
+			printf("%-2d*%-2d=%-3d ", j, i, j*i);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	print(num);
+	return 0;
+}
