bd689c40502803117b55a4c0642e32871751024b
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 22:37:05 +0800

    Number
    Output each bit of an integer.

---
+++ Number.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void print(int num)
+{
+	if (num > 9) {
+		print(num / 10);
+	}
+	printf("%d\n", num%10);
+}
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	print(num);
+	return 0;
+}
