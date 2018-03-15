5acdaac9de1d0bd59ae4d7d97383c2d584fc3a55
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-15 22:14:33 +0800

    Count one bits
    The number of 1 in the parameter binary.

---
+++ Count_one_bits.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int count_one_bits(unsigned int value)
+{
+	// 返回 1的位数
+	int count = 0;
+	while (value) {
+		value = value & (value - 1);
+		count++;
+	}
+	return count;
+}
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	int count = count_one_bits(num);
+	printf("%d", count);
+	return 0;
+}
