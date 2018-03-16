ef821e1b855a75f014690f57684a766e702ce06b
MrHHHH <34007808+MrHHHH@users.noreply.github.com>
2018-03-16 23:12:40 +0800

    计算并输出两点间的距离

---
+++ Distance.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void fun(char *x, char *y)
+{
+    if (*x > *y)
+    {
+        char tmp = *x;
+        *x = *y;
+        *y = tmp;
+    }
+}
+int main()
+{
+    char a, b, c;
+    while (scanf("%c%c%c", &a, &b, &c) != EOF)
+    {
+        getchar();
+        fun(&a, &b);
+        fun(&a, &c);
+        fun(&b, &c);
+        printf("%c %c %c\n", a, b, c);
+    }
+    return 0;
+}
